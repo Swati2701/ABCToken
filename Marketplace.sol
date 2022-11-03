844e23a62706eb36ce0f792ff879da86764d91f7
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2022-11-03 16:38:22 +0530

    Uploaded marketplace contract

---
+++ Marketplace.sol
@@ -0,0 +1,47 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721Receiver.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
+// import ""
+
+contract Marketplace{
+    IERC20 private immutable addrToken;
+    
+    address public owner;
+    
+    mapping(address => mapping(uint256 => bool)) public offerExist;
+    mapping(address=> mapping(uint256 => uint256)) public offerId;
+    
+    constructor(IERC20 _token){
+        addrToken = _token;
+        owner = msg.sender;
+    }
+    
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not called by owner");
+        _;
+    }
+
+
+    function makeOffer(address addrNFT, uint256 tokenId) external{
+        require(addrToken.balanceOf(msg.sender)!=0, "User must have ABCToken");
+        offerId[addrNFT][tokenId]++;
+    }
+
+    function rejectOffer(uint256 _offerId) external{
+        require(!(offerExist[msg.sender][_offerId]), "offer cannot be rejected");
+        offerExist[msg.sender][_offerId] = false;
+    }
+
+    function acceptOffer(uint256 _offerId) external onlyOwner{
+        require(offerExist[msg.sender][_offerId], "Already accepted offer");
+        offerExist[msg.sender][_offerId] = true;
+    }
+
+    function fetchOffer(uint256 _offerValue) external view returns(bool){
+        return (offerExist[msg.sender][_offerValue]);
+    }
+}
